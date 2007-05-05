3db9a4784e6f9ca8ff4cf203f2c3f9ac12eed661
neoxx <neoxx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-05-05 09:54:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/featuring-countcomments/trunk@12205 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- featuring-countcomments.php
+++ featuring-countcomments.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Featuring CountComments
 Plugin URI: http://www.neotrinity.at/projects/
-Description: Count the number of comments by authornames. - Attention! This means, that your commenters have to be registered and logged in to comment! This will not work in weblogs where everyone can comment! original code by Martijn van der Kwast (http://www.stilglog.com/wordpress-plugins/count-comments/)
+Description: Counts the number of comments by authornames. - Attention! This means, that your commenters have to be registered and logged in to comment! This will not work in weblogs where everyone can comment! original code by Martijn van der Kwast (http://www.stilglog.com/wordpress-plugins/count-comments/)
 Author: Bernhard Riedl
 Version: 0.11 (beta)
 Author URI: http://www.neotrinity.at
