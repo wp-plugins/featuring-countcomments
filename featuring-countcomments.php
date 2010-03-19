9dd961e2c439d70d0df097922c527f95ae53bb0e
neoxx <neoxx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-19 17:41:20 +0000

    freeze version for wordpress minor 2.8
    git-svn-id: https://plugins.svn.wordpress.org/featuring-countcomments/tags/wordpress minor 2.8@219666 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ featuring-countcomments.php
@@ -0,0 +1,244 @@
+<?php
+
+/*
+Plugin Name: Featuring CountComments
+Plugin URI: http://www.neotrinity.at/projects/
+Description: Counts the number of comments by user IDs or author names (display name). - Attention! This means, that your commenters have to be registered and logged in to comment! Thus, it will not work in weblogs where anonymous comments are allowed! original code by Martijn van der Kwast
+Author: Bernhard Riedl
+Version: 0.22
+Author URI: http://www.neotrinity.at
+*/
+
+/*  Copyright 2006-2009  Bernhard Riedl  (email : neo@neotrinity.at)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+function fcc_wp_head() {
+	echo("<meta name=\"Featuring CountComments\" content=\"0.22\" />\n");
+}
+
+$_fcc_cache=array();
+
+function _fcc_precache_posts_counts($post_ID)
+{
+	global $wpdb;
+	global $_fcc_cache;
+
+	$post_ID=intval($post_ID);
+	if (!$post_ID) {
+		return;
+	}
+
+	// get all commenters for current post
+	$q = "SELECT DISTINCT $wpdb->comments.user_id FROM $wpdb->comments " .
+		"WHERE $wpdb->comments.comment_post_ID = $post_ID AND $wpdb->comments.user_id <> '' ";
+
+	$authors = $wpdb->get_col($q);
+	if (!$authors) {
+		return;
+	}
+
+	$aq='';
+	foreach ($authors as $author) {
+		$_fcc_cache[$author] = -1;
+		$aq.="'".$wpdb->escape($author)."', ";
+	}
+	$aq=substr($aq, 0, -2);
+
+	$q = "SELECT $wpdb->comments.user_id, COUNT(*) AS count FROM $wpdb->comments " .
+		"WHERE $wpdb->comments.comment_approved = '1' and $wpdb->comments.user_id in ( $aq ) " .
+		"GROUP BY $wpdb->comments.user_id";
+
+	$rows = $wpdb->get_results($q);
+
+	// save results
+	foreach ($rows as $row) {
+		$_fcc_cache[$row->user_id] = $row->count;
+	}
+
+}
+
+function _fcc_cache_user_counts($user_id)
+{
+	global $wpdb;
+	global $_fcc_cache;
+
+	if (!isset($_fcc_cache[$user_id])) {
+
+		// get global comment counts for all mentioned authors
+		$q = "SELECT $wpdb->comments.user_id, COUNT(*) AS count FROM $wpdb->comments " .
+			"WHERE $wpdb->comments.comment_approved = '1' and $wpdb->comments.user_id = ".$user_id." ".
+			"GROUP BY $wpdb->comments.user_id";
+
+		$rows = $wpdb->get_results($q);
+
+		// save results
+		if ($rows) {
+			$_fcc_cache[$rows[0]->user_id] = $rows[0]->count;
+		}
+
+		// no results - maybe users deleted account
+		else {
+			$_fcc_cache[$rows[0]->user_id] = -1;
+		}
+
+	}
+}
+
+function _fcc_get_comment_authorID($author) {
+	global $wpdb;
+
+	$q = "SELECT $wpdb->users.ID FROM $wpdb->users " .
+	     "WHERE $wpdb->users.display_name = '$author' ORDER BY $wpdb->users.ID DESC";
+
+	$authors = $wpdb->get_col($q);
+	if (!$authors) {
+		return -1;
+	}
+	else {
+		return $authors[0];
+	}
+}
+
+/*
+ * Returns the number of comments made by the author of this comment
+ * parameters:
+ * $comment: current comment structure.
+ */
+
+function fcc_get_comment_count($comment)
+{
+	global $_fcc_cache;
+
+	if (empty($comment->user_id)) {
+		return 0;
+	}
+
+	if (!isset($_fcc_cache[$comment->user_id])) {
+		_fcc_precache_posts_counts($comment->comment_post_ID);
+	}
+
+	return $_fcc_cache[$comment->user_id];
+}
+
+/*
+ * Displays the number of comments made by the author of the current comment in the comment loop
+ * parameters:
+ * $comment: current comment structure.
+ * $zero, $one, $more: %c get replaced with the number of comments.
+ */
+
+function fcc_comment_count($zero='0 comments', $one='1 comment', $more='%c comments')
+{
+	global $comment;
+
+	$num = fcc_get_comment_count($comment);
+
+	if ($num == 0) {
+		print str_replace('%c', $num, $zero);
+	} else if ($num == 1) {
+		print str_replace('%c', $num, $one);
+	} else if( $num > 1) {
+		print str_replace('%c', $num, $more);
+	}
+}
+
+/*
+ * Returns the number of comments made by someone with this display name
+ * parameters:
+ * $author: name of user to count comments for
+ */
+
+function fcc_get_count_comments_author($author)
+{
+	return fcc_get_count_comments_authorID( _fcc_get_comment_authorID($author) );
+}
+
+/*
+ * Returns the number of comments made by someone with this id
+ * parameters:
+ * $authorID: id of user to count comments for
+ */
+
+function fcc_get_count_comments_authorID($authorID)
+{
+	global $_fcc_cache;
+
+	if (!isset($_fcc_cache[$authorID])) {
+		_fcc_cache_user_counts($authorID);
+	}
+
+	return $_fcc_cache[$authorID];
+}
+
+/*
+ * Displays the number of comments made by the author who is currently logged in
+ * parameters:
+ * $zero, $one, $more: %c get replaced with the number of comments.
+ */
+
+function fcc_count_comments_author($zero='0 comments', $one='1 comment', $more='%c comments')
+{
+	global $user_ID;
+
+	$num = fcc_get_count_comments_authorID($user_ID);
+
+	if ($num == 0) {
+		print str_replace('%c', $num, $zero);
+	} else if ($num == 1) {
+		print str_replace('%c', $num, $one);
+	} else if( $num > 1) {
+		print str_replace('%c', $num, $more);
+	}
+}
+
+/*
+ * Displays the number of comments made by a specified author with this display name
+ * parameters:
+ * $zero, $one, $more: %c get replaced with the number of comments
+ * $author: the authors nickname
+ */
+
+function fcc_count_comments_by_author($zero='0 comments', $one='1 comment', $more='%c comments', $author)
+{
+	fcc_count_comments_by_authorID($zero, $one, $more, _fcc_get_comment_authorID($author) );
+}
+
+/*
+ * Displays the number of comments made by a specified author with this ID
+ * parameters:
+ * $comment: current comment structure.
+ * $zero, $one, $more: %c get replaced with the number of comments
+ * $authorID: the authors id
+ */
+
+function fcc_count_comments_by_authorID($zero='0 comments', $one='1 comment', $more='%c comments', $authorID)
+{
+	$num = fcc_get_count_comments_authorID($authorID);
+
+	if ($num == 0) {
+		print str_replace('%c', $num, $zero);
+	} else if ($num == 1) {
+		print str_replace('%c', $num, $one);
+	} else if( $num > 1) {
+		print str_replace('%c', $num, $more);
+	}
+}
+
+add_action('admin_head', 'fcc_wp_head');
+add_action('wp_head', 'fcc_wp_head');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,81 @@
+=== Featuring CountComments ===
+Contributors: neoxx
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=neo%40neotrinity%2eat&item_name=neotrinity%2eat&no_shipping=1&no_note=1&tax=0&currency_code=USD&bn=PP%2dDonationsBF&charset=UTF%2d8
+Tags: count, comment, comments, author, authors
+Requires at least: 1.5
+Tested up to: 3.0
+Stable tag: trunk
+
+Counts the number of comments by authornames.
+
+== Description ==
+
+Counts the number of comments by user IDs or author names (display name).
+
+**Attention! This means, that your commenters have to be registered and logged in to comment!** Thus, it will not work in weblogs where anonymous comments are allowed!
+
+Original code by Martijn van der Kwast.
+
+== Installation ==
+
+1. Copy the `featuring-countcomments` directory into your WordPress plugins directory (usually wp-content/plugins). Hint: With WordPress 2.7 and higher you can conduct this step within your Admin Menu.
+
+2. In the WordPress Admin Menu go to the Plugins tab and activate the Featuring CountComments plugin.
+
+3. To display the comment count of a certain author, add this in the comment loop
+
+	(`$comment` must be globally defined).
+	`<?php fcc_comment_count(); ?>`
+
+	Example for comments.php in the Kubrick theme:
+	Find
+
+	`<cite><?php comment_author_link</cite>`
+
+	and insert this in that line:
+
+	`<?php fcc_comment_count('','(1)','(%c)'); ?>`
+
+4. Add additional [function calls](http://wordpress.org/extend/plugins/featuring-countcomments/other_notes/) according to your desires.
+
+5. Be happy and celebrate! (and maybe you want to add a link to [http://www.neotrinity.at/projects/](http://www.neotrinity.at/projects/))
+
+== Functions ==
+
+= Functions for Post or Comment Templates =
+
+* `function fcc_get_comment_count($comment)`
+ - Returns the number of comments made by the author of this comment
+ - parameters: $comment: current comment structure
+
+* `function fcc_comment_count($zero='0 comments', $one='1 comment', $more='%c comments')`
+ - Displays the number of comments made by the author of the current comment in the comment loop
+ - parameters: $zero, $one, $more: %c get replaced with the number of comments.
+
+= Functions for Author Pages =
+
+* `function fcc_get_count_comments_author($author)`
+ - Returns the number of comments made by someone with this display name
+ - parameters: $author: name of user to count comments for
+
+* `function fcc_get_count_comments_authorID($authorID)`
+ - same as above, but based on ID instead of the author's name
+ - parameters: $authorID: name of user to count comments for
+
+* `function fcc_count_comments_author($zero='0 comments', $one='1 comment', $more='%c comments')`
+ - Displays the number of comments made by the author who is currently logged in
+ - parameters: $zero, $one, $more: %c get replaced with the number of comments.
+
+* `function fcc_count_comments_by_author($zero='0 comments', $one='1 comment', $more='%c comments', $author)`
+ - Displays the number of comments made by a specified author with this display name
+ - parameters: $zero, $one, $more: %c get replaced with the number of comments, $author: the authors display name
+
+* `function fcc_count_comments_by_authorID($zero='0 comments', $one='1 comment', $more='%c comments', $authorID)`
+ - same as above, but based on ID instead of the author's name
+ - parameters: $zero, $one, $more: %c get replaced with the number of comments, $authorID: the authors id
+
+== Remarks ==
+
+* Authors' nicknames (display names) and ids can be used to query for commenters. Though, the internal structure is based on the authors' id and will therefore only produce results of comments, which have been written by authors who have been logged in at the time of writing the comment.
+* Already queried results are re-used within a single page-call to avoid performing too many queries. This results in increased performance.
+* Nevertheless, if the number of comments gets really big, it would be better to cache the counts in the database instead of calculating them on every page-call.
